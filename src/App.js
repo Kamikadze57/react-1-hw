91d731ab65baf9a61841d8b4393cf3effbb6283a
Kamikadze57 <143871458+Kamikadze57@users.noreply.github.com>
2025-09-13 19:27:21 +0300

    finish

--- src/App.js
+++ src/App.js
@@ -8,15 +8,17 @@ let b = 7;
 function App() {
   return (
     <div className="app">
-      <header className="app-header">
+      <header className="app__header">
         <h1 className="header__title">{name}</h1>
         <p className="header__text">{a + b}</p>
       </header>
       <main>
         <p className="text">Ласкаво прошу до першого проекту з React!</p>
         <img className="app__logo" src={logo} alt="React" />
+        <div className="lists__box">
         <FavoriteSites />
         <Colors />
+        </div>
       </main>
     </div>
   );
--- src/components/favoriteSites.jsx
+++ src/components/favoriteSites.jsx
@@ -3,12 +3,12 @@ import { favoriteSitesData } from "../data/favoriteSites";
 export const FavoriteSites = () => {
   console.log(favoriteSitesData);
   return (
-    <ul className="site_list">
+    <ul className="site__list">
       {favoriteSitesData.map((site) => {
         return (
-          <li className="site_item" key={site.id}>
-            <h2 className="site-item_name">{site.name}</h2>
-            <a className="site-item_link" href={site.url}>
+          <li className="site__item" key={site.id}>
+            <h2 className="site-item__name">{site.name}</h2>
+            <a className="site-item__link" href={site.url}>
               Перейти на сайт
             </a>
           </li>
